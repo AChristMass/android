f0557c7114172c64c112a68e5d5b7dc6c92fae21
massicilia16 <57214437+massicilia16@users.noreply.github.com>
2020-03-22 13:39:55 +0100

    Update IfcDimensions.java

--- app/src/main/java/fr/igm/robotmissions/objects/ifc/IfcDimensions.java
+++ app/src/main/java/fr/igm/robotmissions/objects/ifc/IfcDimensions.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 public class IfcDimensions implements Serializable {
 
+    //declarer les dimentions des murs pour calculer la dimension de l'ifc
     private double xMin;
     private double xMax;
     private double yMin;
@@ -25,10 +26,11 @@ public class IfcDimensions implements Serializable {
         return yMin;
     }
 
-
+//calcul de la largeur de la piece
     public double getWidth(){
         return Math.abs(xMax-xMin);
     }
+//calcul de la longueur de la piece
     public double getHeight(){
         return Math.abs(yMax-yMin);
     }
